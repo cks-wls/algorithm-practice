97f04d05f59f7d6f0a5d272af70ff2917ecedd5d
cks-wls <slo0521@naver.com>
2026-05-08 13:53:46 +0900

    [Lv.0] 배열의 평균값

---
+++ programmers/Lv.0/배열의 평균값.js
@@ -0,0 +1,9 @@
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+// 정답이 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
+function solution(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+}
