1f177b22d42369ae97a685b8f18db90548fe5e44
cks-wls <slo0521@naver.com>
2026-05-11 15:10:37 +0900

    [Lv.0] 짝수 홀수 개수

---
+++ programmers/Lv.0/짝수 홀수 개수.js
@@ -0,0 +1,16 @@
+// 정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+function solution(num_list) {
+  const answer = [];
+  let odds_count = 0;
+  let evens_count = 0;
+  for (let i = 0; i < num_list.length; i++) {
+    if (num_list[i] % 2 === 1) {
+      odds_count++;
+    } else {
+      evens_count++;
+    }
+  }
+  answer.push(evens_count);
+  answer.push(odds_count);
+  return answer;
+}
