3b59c59844e594c4869a003c26e5d477fc962503
cks-wls <slo0521@naver.com>
2026-07-25 18:41:34 +0900

    [Lv.0] 0 떼기

---
+++ programmers/Lv.0/0 떼기.js
@@ -0,0 +1,9 @@
+// 정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
+function solution(n_str) {
+  let arr = n_str.split("");
+  while (arr[0] === "0") {
+    arr.shift();
+  }
+
+  return arr.join("");
+}
