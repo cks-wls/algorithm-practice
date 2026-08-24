25176dd367027461d6be65fec39836c1a4dddb1b
cks-wls <slo0521@naver.com>
2026-08-24 19:34:29 +0900

    [Lv.0] 특정 문자 제거하기

---
+++ programmers/Java/Lv.0/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        for(String val : arr){
+            if(!(val.equals(letter))){
+                answer += val;
+            }
+        }
+        return answer;
+    }
+}
