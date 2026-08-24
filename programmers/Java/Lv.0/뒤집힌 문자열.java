5f3cacaf07edd896345f32696bf05cfb7cb019f5
cks-wls <slo0521@naver.com>
2026-08-24 19:24:52 +0900

    [Lv.0] 뒤집힌 문자열

--- programmers/Java/Lv.0/뒤집힌 문자열.java
+++ programmers/Java/Lv.0/뒤집힌 문자열.java
@@ -1,3 +1,4 @@
+// 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
 class Solution {
     public String solution(String my_string) {
         String[] arr = my_string.split("");
