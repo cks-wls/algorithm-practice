583d1a6a1af7bd2bf24fea2dfe00766dac84a303
cks-wls <slo0521@naver.com>
2026-09-01 17:07:08 +0900

    [Lv.0] 문자열 돌리기

---
+++ programmers/Java/Lv.0/문자열 돌리기.java
@@ -0,0 +1,14 @@
+// 문자열 str이 주어집니다.
+// 문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i = 0; i < a.length(); i++){
+            char c = a.charAt(i);
+            System.out.println(c);
+        }
+    }
+}
