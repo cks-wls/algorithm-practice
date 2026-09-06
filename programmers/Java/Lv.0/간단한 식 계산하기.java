c7dc80e2384f87909e126df1d5e99d7f89b21e05
cks-wls <slo0521@naver.com>
2026-09-06 16:20:01 +0900

    [Lv.0] 간단한 식 계산하기

---
+++ programmers/Java/Lv.0/간단한 식 계산하기.java
@@ -0,0 +1,13 @@
+// 문자열 binomial이 매개변수로 주어집니다. binomial은 "a op b" 형태의 이항식이고 a와 b는 음이 아닌 정수, op는 '+', '-', '*' 중 하나입니다. 주어진 식을 계산한 정수를 return 하는 solution 함수를 작성해 주세요.
+class Solution {
+    public int solution(String binomial) {
+        String[] arr = binomial.split(" ");
+        int answer = 0;
+        switch(arr[1]){
+                case "+" -> answer = Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]);
+                case "-" -> answer = Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]);
+                case "*" -> answer = Integer.parseInt(arr[0]) * Integer.parseInt(arr[2]);
+        }
+        return answer;
+    }
+}
