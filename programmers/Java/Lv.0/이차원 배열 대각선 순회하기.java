b66f55e88aea003ccd435c6a1fd87d7b624a3926
cks-wls <slo0521@naver.com>
2026-09-16 13:55:36 +0900

    [Lv.0] 이차원 배열 대각선 순회하기

---
+++ programmers/Java/Lv.0/이차원 배열 대각선 순회하기.java
@@ -0,0 +1,13 @@
+// 2차원 정수 배열 board와 정수 k가 주어집니다.
+// i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 solution 함수를 완성해 주세요.
+class Solution {
+    public int solution(int[][] board, int k) {
+        int sum = 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(i + j <= k) sum += board[i][j];
+            }
+        }
+        return sum;
+    }
+}
