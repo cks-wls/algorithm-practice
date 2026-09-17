e099d008521e7d155d38b532ab373fc3a128c82b
cks-wls <slo0521@naver.com>
2026-09-17 18:07:06 +0900

    [Lv.0] 문자열 섞기

---
+++ programmers/Java/Lv.0/문자열 섞기.java
@@ -0,0 +1,14 @@
+// 길이가 같은 두 문자열 str1과 str2가 주어집니다.
+// 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        for(int i = 0; i < str1.length(); i++){
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+            answer += c1;
+            answer += c2;
+        }
+        return answer;
+    }
+}
