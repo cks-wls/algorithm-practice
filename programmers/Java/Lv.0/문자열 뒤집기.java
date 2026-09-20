95a0db0334ee6301d3f80167cedf154c383df301
cks-wls <slo0521@naver.com>
2026-09-20 12:46:42 +0900

    [Lv.0] 문자열 뒤집기

---
+++ programmers/Java/Lv.0/문자열 뒤집기.java
@@ -0,0 +1,18 @@
+// 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String str = "";
+        for(int i = e; i >= s; i--){
+            str += my_string.charAt(i);
+        }
+        String answer = "";
+        for(int i = 0; i < s; i++){
+            answer += my_string.charAt(i);
+        }
+        answer += str;
+        for(int i = e + 1; i < my_string.length(); i++){
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
