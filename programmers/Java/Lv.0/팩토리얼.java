f9901477f7ae15160b0a9869f3371acccb77e6b8
cks-wls <slo0521@naver.com>
2026-09-22 15:26:28 +0900

    [Lv.0] 팩토리얼

---
+++ programmers/Java/Lv.0/팩토리얼.java
@@ -0,0 +1,26 @@
+// - i! ≤ n
+// i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        while(true){
+            if(factorial(answer) > n){
+                return answer - 1;
+            }else if(factorial(answer) == n){
+                return answer;
+            }else{
+                answer ++;
+            }
+        }
+    }
+    static int factorial(int num){
+        if(num == 0){
+            return 1;
+        }
+        int total = 1;
+        for(int i = 1; i <= num; i++){
+            total *= i;
+        }
+        return total;
+    }
+}
